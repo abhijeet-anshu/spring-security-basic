8b454e0ec05436e527cda5495a9a2cdeeb3da46a
Abhijeet Baranwal <abhiva.adi@gmail.com>
2021-05-29 17:38:54 +0530

    lesson 2, static password list 2-2

---
+++ guest-app/src/test/java/boot/spring/guestapp/GuestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.spring.guestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ guest-services/src/test/java/boot/spring/guestservices/GuestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.spring.guestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
