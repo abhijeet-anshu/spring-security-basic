4d492c9cca1fe6d0ee94b3d47342107c19c8c5ba
Abhijeet Baranwal <abhiva.adi@gmail.com>
2021-05-30 19:00:10 +0530

    //OAUTH2 implemented (now deprecated by spring security)
    //http://tuxgraphics.org/toolbox/base64-javascript.html
    // guest_app:secret
    // => Z3Vlc3RfYXBwOnNlY3JldA==
    
    http --form POST localhost:8100/oauth/token Authorization:"Basic Z3Vlc3RfYXBwOnNlY3JldA==" grant_type=client_credentials
    
    HTTP/1.1 200
    Cache-Control: no-store
    Connection: keep-alive
    Content-Type: application/json;charset=UTF-8
    Date: Sun, 30 May 2021 13:17:16 GMT
    Keep-Alive: timeout=60
    Pragma: no-cache
    Transfer-Encoding: chunked
    X-Content-Type-Options: nosniff
    X-Frame-Options: DENY
    X-XSS-Protection: 1; mode=block
    
    {
        "access_token": "96b97a8a-ce8b-40dd-bccb-b847ba44bcb8",
        "expires_in": 43199,
        "scope": "READ_ALL_GUESTS WRITE_GUEST UPDATE_GUEST",
        "token_type": "bearer"
    }
    
    //verify token above
    
    http --form POST localhost:8100/oauth/check_token Authorization:"Bearer 96b97a8a-ce8b-40dd-bccb-b847ba44bcb8" token=96b97a8a-ce8b-40dd-bccb-b847ba44bcb8
    
    HTTP/1.1 200
    Cache-Control: no-cache, no-store, max-age=0, must-revalidate
    Connection: keep-alive
    Content-Type: application/json
    Date: Sun, 30 May 2021 13:23:46 GMT
    Expires: 0
    Keep-Alive: timeout=60
    Pragma: no-cache
    Transfer-Encoding: chunked
    X-Content-Type-Options: nosniff
    X-Frame-Options: DENY
    X-XSS-Protection: 1; mode=block
    
    {
        "active": true,
        "authorities": [
            "ROLE_GUESTS_AUTHORIZED_CLIENT"
        ],
        "client_id": "guest_app",
        "exp": 1622423836,
        "scope": [
            "READ_ALL_GUESTS",
            "WRITE_GUEST",
            "UPDATE_GUEST"
        ]
    }
    
    //http://tuxgraphics.org/toolbox/base64-javascript.html
    // api_audit:secret
    // => YXBpX2F1ZGl0OnNlY3JldA==
    
    http --form POST localhost:8100/oauth/token Authorization:"Basic YXBpX2F1ZGl0OnNlY3JldA==" grant_type=client_credentials
    
    HTTP/1.1 200
    Cache-Control: no-store
    Connection: keep-alive
    Content-Type: application/json;charset=UTF-8
    Date: Sun, 30 May 2021 13:26:27 GMT
    Keep-Alive: timeout=60
    Pragma: no-cache
    Transfer-Encoding: chunked
    X-Content-Type-Options: nosniff
    X-Frame-Options: DENY
    X-XSS-Protection: 1; mode=block
    
    {
        "access_token": "f9c9cd96-5df4-4346-b19f-99856954f392",
        "expires_in": 43200,
        "scope": "READ_ALL_GUESTS",
        "token_type": "bearer"
    }

--- guest-services/src/main/java/boot/spring/guestservices/GuestServicesApplication.java
+++ guest-services/src/main/java/boot/spring/guestservices/GuestServicesApplication.java
@@ -2,8 +2,10 @@ package boot.spring.guestservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class GuestServicesApplication {
 
 	public static void main(String[] args) {
